149d7431d22fb7a1aa19c38e2ba5b75ef864e9a1
AnilBabu01 <anilbabu3245@gmail.com>
2026-05-01 18:47:25 +0530

    working on

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://kolkataff.cloud/sitemap.xml",
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://kolkataff.cloud",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://kolkataff.cloud/tips",
+      lastModified: new Date(),
+    },
+  ];
+}
